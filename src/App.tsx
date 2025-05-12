cbe8bc285c62455c2d656ed430f6fc2400df62e6
514sid <140138716+514sid@users.noreply.github.com>
2025-05-12 21:28:44 +0400

    refactor: update App component to use SectionContainer and scale layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import playlistData from './assets/playlist_data.json'
 import type { Section } from './types'
-import { SectionContainer } from './Section'
+import { SectionContainer } from './SectionContainer'
+import { useScaleLayout } from './hooks/useScaleLayout'
 
 export const App = () => {
+    const config = useMemo(() => {
+        return {
+            playbackStartedAt: 1747054972,
+            scaleLayout: true,
+        }
+    }, [])
+		
     const [sections, setSections] = useState<Section[]>([])
 
     useEffect(() => {
         setSections(playlistData.sections)
     }, [])
 
+    const scale = useScaleLayout(playlistData, config.scaleLayout)
+
     if(sections.length === 0) {
         return <div>Loading...</div>
     }
 
     return (
-        <>
+        <div className='bg-black w-screen h-screen overflow-hidden'>
             {
                 sections.map((section, index) => (
-                    <SectionContainer key={index} section={section} />
+                    <SectionContainer key={index} section={section} scale={scale} playbackStartedAt={ config.playbackStartedAt }/>
                 ))
             }
-        </>
+        </div>
     )
 }
