2d0bc5a7db9f7438e4e111d97b9721e9c9af883c
514sid <140138716+514sid@users.noreply.github.com>
2025-05-13 12:51:03 +0400

    refactor

--- src/hooks/useMediaSequence.ts
+++ src/hooks/useMediaSequence.ts
@@ -3,32 +3,43 @@ import type { MediaSequenceState, Section } from '../types'
 import { useSectionMediaItems } from './useSectionMediaItems'
 import { calculateMediaSequenceState } from '../utils/calculateCurrentMediaState'
 import { updateMediaItemsState } from '../utils/updateMediaItemsState'
+import { hasStateChanged } from '../utils/hasStateChanged'
 
 const UPDATE_INTERVAL_MS = 50
 
 export const useMediaSequence = (section: Section, playbackStartTime: number) => {
-    const { mediaItems, setMediaItems, totalDurationRef } = useSectionMediaItems(section.items)
-    const state = useRef<MediaSequenceState>(calculateMediaSequenceState(mediaItems, playbackStartTime, totalDurationRef.current))
+    const {
+        mediaItems,
+        setMediaItems,
+        totalDurationRef
+    } = useSectionMediaItems(section.items)
 	
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            const newState = calculateMediaSequenceState(mediaItems, playbackStartTime, totalDurationRef.current)
+    const stateRef = useRef<MediaSequenceState>(
+        calculateMediaSequenceState(mediaItems, playbackStartTime, totalDurationRef.current)
+    )
 
-            const stateChanged = newState.currentIndex !== state.current.currentIndex
-							  || newState.shouldPreloadNext !== state.current.shouldPreloadNext
-							  || newState.nextItemIndex !== state.current.nextItemIndex
+    const updateSequenceState = () => {
+        const newState = calculateMediaSequenceState(
+            mediaItems,
+            playbackStartTime,
+            totalDurationRef.current
+        )
 
-            if (stateChanged) {
-                state.current = newState
-                const updatedItems = updateMediaItemsState(mediaItems, state.current)
+        if (hasStateChanged(stateRef.current, newState)) {
+            stateRef.current = newState
+            const updatedItems = updateMediaItemsState(mediaItems, newState)
 
-                if (updatedItems !== mediaItems && state.current) {
-                    setMediaItems(updatedItems)
-                }
+            if (updatedItems !== mediaItems) {
+                setMediaItems(updatedItems)
             }
-        }, UPDATE_INTERVAL_MS)
+        }
+    }
+
+    useEffect(() => {
+        const intervalId = setInterval(updateSequenceState, UPDATE_INTERVAL_MS)
 
         return () => clearInterval(intervalId)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [mediaItems, playbackStartTime, setMediaItems, totalDurationRef])
 
     return { mediaItems }
