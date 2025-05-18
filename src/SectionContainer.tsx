11ce6479db1ee1ba459ccfd635ac0ca9ee750418
514sid <140138716+514sid@users.noreply.github.com>
2025-05-18 23:19:02 +0400

    add container wrapper

--- src/SectionContainer.tsx
+++ src/SectionContainer.tsx
@@ -1,11 +1,24 @@
 import type { Section } from './types'
 import { useMediaSequence } from './hooks/useMediaSequence'
 import { MediaRenderer } from './MediaRenderer'
+import { useMemo } from 'react'
 
 export const SectionContainer = ({ section, scale, elapsedSinceStart }: { section: Section, scale: number, elapsedSinceStart: number }) => {	
     const { mediaItems } = useMediaSequence(section, elapsedSinceStart)
 	
+    const style = useMemo(() => ({
+        position: 'fixed' as const,
+        left: Math.floor(section.position.x * scale),
+        top: Math.floor(section.position.y * scale),
+        width: Math.floor(section.position.width * scale),
+        height: Math.floor(section.position.height * scale),
+        overflow: 'hidden' as const,
+        zIndex: section.position.z_index,
+    }), [section.position, scale])
+
     return (
-        <MediaRenderer section={section} mediaItems={mediaItems} scale={ scale }/>
+        <div style={ style }>
+            <MediaRenderer mediaItems={mediaItems}/>
+        </div>
     )
 }
