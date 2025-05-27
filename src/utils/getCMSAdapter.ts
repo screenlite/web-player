47e1c6857bd27e836b3456cdc7b36ef1443ab221
514sid <140138716+514sid@users.noreply.github.com>
2025-05-27 21:57:44 +0400

    feat: add ScreenliteAdapter support in getCMSAdapter function

--- src/utils/getCMSAdapter.ts
+++ src/utils/getCMSAdapter.ts
@@ -1,5 +1,6 @@
 import { NetworkFileAdapter } from '../adapters/NetworkFileAdapter'
 import { GarlicHubAdapter } from '../adapters/GarlicHubAdapter'
+import { ScreenliteAdapter } from '../adapters/ScreenliteAdapter'
 
 export const getCMSAdapter = (adapter: string | null) => {
     const adapterEnv = adapter || import.meta.env.VITE_CMS_ADAPTER
@@ -9,6 +10,8 @@ export const getCMSAdapter = (adapter: string | null) => {
         return new NetworkFileAdapter(url)
     } else if (adapterEnv === 'GarlicHub') {
         return new GarlicHubAdapter(url)
+    } else if (adapterEnv === 'Screenlite') {
+        return new ScreenliteAdapter(url)	
     } else {
         throw new Error(`Unknown CMS adapter: ${adapterEnv}`)
     }
